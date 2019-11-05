ed9e85a3a6ffbca556e50fd31a12ff810a112bea
xianzhixianzhixian <MrRobotFan@163.com>
2019-11-05 22:10:56 +0800

    用户中心搭建

---
+++ seckill-user/src/test/java/com/seckill/user/SeckillUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seckill.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
