ab406129f4317cf7bf739fbf2bdcbfa1a029918c
xianzhixianzhixian <MrRobotFan@163.com>
2019-12-22 23:07:37 +0800

    事件状态

---
+++ seckill-common/src/main/java/com/seckill/common/constant/OrderEventType.java
@@ -0,0 +1,8 @@
+package com.seckill.common.constant;
+
+/**
+ * 订单事件状态
+ * @author xianzhixianzhixian on 2019/12/22 23:04
+ */
+public class OrderEventType extends EventType {
+}
---
+++ seckill-common/src/main/java/com/seckill/common/constant/SeckillEventType.java
@@ -0,0 +1,8 @@
+package com.seckill.common.constant;
+
+/**
+ * 秒杀事件状态
+ * @author xianzhixianzhixian on 2019/12/22 23:04
+ */
+public class SeckillEventType extends EventType {
+}
