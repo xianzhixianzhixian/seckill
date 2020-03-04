738e780139f290ecaef01214a4a3a010012f43ef
xianzhixianzhixian <MrRobotFan@163.com>
2020-03-04 15:30:36 +0800

    秒杀系统数据库连接池改为Druid 1.1.21

--- manager-product/src/main/java/com/seckill/manager/product/ManagerProductApplication.java
+++ manager-product/src/main/java/com/seckill/manager/product/ManagerProductApplication.java
@@ -1,16 +1,9 @@
 package com.seckill.manager.product;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -19,24 +12,6 @@ public class ManagerProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerProductApplication.class, args);
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return new DataSource();
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
     }
 
 }
--- manager-user/src/main/java/com/seckill/manager/user/ManagerUserApplication.java
+++ manager-user/src/main/java/com/seckill/manager/user/ManagerUserApplication.java
@@ -1,15 +1,8 @@
 package com.seckill.manager.user;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -17,24 +10,6 @@ public class ManagerUserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagerUserApplication.class, args);
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return new DataSource();
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
     }
 
 }
